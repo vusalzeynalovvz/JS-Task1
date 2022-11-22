c602b7190f4f8806a86489177562259e4e52ba7f
vusalzeynalovvz <112488906+vusalzeynalovvz@users.noreply.github.com>
2022-11-22 21:05:22 +0400

    Add files via upload

---
+++ JS-Task2/main.js
@@ -0,0 +1,33 @@
+
+//     Task-1
+
+// function Confirm(){
+//     let content;
+//     if(confirm('Click button')===true){
+//         content="Xos gelmisiniz"
+//         document.body.style.backgroundColor='blue';
+//     } 
+//     else{
+//         content="yanlis sorgu";
+//         document.body.style.backgroundColor='red'
+//     }
+//     document.getElementById("Text").innerHTML = content;
+// }
+
+//             Task-2
+
+// function P229() {
+//     let age = prompt("Please enter your name","18");
+//     if (age<18) {
+//       document.getElementById("Age").innerHTML ="Giris ede bilmessiniz"
+//     }
+//     else{
+//         document.getElementById("Age").innerHTML="Xos gelmisiniz"
+//     }
+//   }
+
+        // Task-3
+// function P229(){
+      
+// }
+
